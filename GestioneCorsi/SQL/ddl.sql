75c595d7e9bb3f40472bc0a0f97a11c92a61c1d8
ffv8 <favotto.federico@gmail.com>
2021-11-30 15:28:46 +0100

    Aggiunta tabella docente

--- GestioneCorsi/SQL/ddl.sql
+++ GestioneCorsi/SQL/ddl.sql
@@ -7,3 +7,11 @@ password varchar2(1024) not null,
 constraint p_cod_admin primary key(cod_admin),
 constraint u_username unique(username)
 );
+
+create table docente(
+nome_docente varchar2(30) not null,
+cognome_docente varchar2(30) not null,
+cod_docente int,
+cv_docente varchar2(1024) not null,
+constraint p_cod_docente primary key(cod_docente)
+);
