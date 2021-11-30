b7c8d27e134130f7d77d4411aa50c56ed5bffabb
canzian0 <canzianalessandro@gmail.com>
2021-11-30 17:31:03 +0100

    aggiunti file SQL
    drop
    insert

---
+++ GestioneCorsi/SQL/drop.sql
@@ -0,0 +1,8 @@
+drop table amministratore cascade constraints;
+drop table corso cascade constraints;
+drop table corsista cascade constraints;
+drop table docente cascade constraints;
+drop table iscrizione cascade constraints;
+
+drop sequence corso_seq;
+drop sequence corsista_seq;
---
+++ GestioneCorsi/SQL/insert.sql
@@ -0,0 +1,8 @@
+insert into docente values(101, 'Tim', 'Cook', 'C,PHP,JAVA,Objective-C');
+insert into docente values(102, 'Mario', 'Rossi', 'C,Python');
+insert into docente values(103, 'Laura', 'Verdi', 'C,JAVA,HTML,Javascript,Typescript');
+insert into docente values(104, 'Marco', 'Bianchi', 'C++,Ruby,SQL');
+insert into docente values(105, 'Marta', 'Neri', 'ASP');
+
+
+insert into amministratore values(1001,'Paolo','De Paoli','root','admin');
