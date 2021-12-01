ee0eb14e9c0ba03c2a9a877d9bf4e6618a9aa8c9
Fede934 <fe.carrar@gmail.com>
2021-12-01 17:08:41 +0100

    Inserite query per il corsista

--- GestioneCorsi/src/main/java/com/gestionecorsi/architecture/dao/DAOConstants.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/architecture/dao/DAOConstants.java
@@ -2,5 +2,9 @@ package com.gestionecorsi.architecture.dao;
 
 public interface DAOConstants {
 
+	String SELECT_CORSISTA="select * from corsista";
+	String DELETE_CORSISTA="delete from corsista where cod_corsista=?";
+	String SELECT_CORSISTA_BYID="select * from corsista where cod_corsista=?";
+	String SELECT_CORSISTA_SEQ="select corsista_seq.nextval from dual";/*prendo il prossimo valore della sequenza*/
 
 }
