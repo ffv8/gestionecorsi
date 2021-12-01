e3f8b964f7966b68b1f337a8839cc4985b2872c1
Fede934 <fe.carrar@gmail.com>
2021-12-01 17:07:58 +0100

    realizzata classe CorsistaIDGenerator

---
+++ GestioneCorsi/src/main/java/com/gestionecorsi/businesscomponent/idgenerator/CorsistaIDGenerator.java
@@ -0,0 +1,59 @@
+package com.gestionecorsi.businesscomponent.idgenerator;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.gestionecorsi.architecture.dao.DAOConstants;
+import com.gestionecorsi.architecture.dao.DAOException;
+import com.gestionecorsi.architecture.dbaccess.DBAccess;
+import com.gestionecorsi.businesscomponent.IDGeneratorInterface;
+
+/*autore Carraro Federico*/
+public class CorsistaIDGenerator implements IDGeneratorInterface,DAOConstants {
+	
+	/*il generatore di id deve essere unico,quindi implemento il singleton*/
+	private static CorsistaIDGenerator istanza;
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet rs;
+	
+	private CorsistaIDGenerator() throws ClassNotFoundException, DAOException, FileNotFoundException, IOException
+	{
+		conn=DBAccess.getConnection();/*stabilisco la connessione col db*/
+		
+		
+	}
+	
+	public static CorsistaIDGenerator getInstance() throws ClassNotFoundException, DAOException, FileNotFoundException, IOException
+	{
+		if(istanza==null)
+			istanza = new CorsistaIDGenerator();
+		return istanza;
+	}
+	
+	@Override
+	public long getNextID() throws DAOException, ClassNotFoundException, IOException {
+		long id=0;
+		try
+		{
+			stmt=conn.createStatement();
+			rs=stmt.executeQuery(SELECT_CORSISTA_SEQ);
+			rs.next();
+			id=rs.getLong(1);
+			
+					
+		}
+		catch(SQLException sql)
+		{
+			throw new DAOException(sql);
+			
+		}
+		
+		return id;
+	}
+
+}
