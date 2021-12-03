fef24b037ef32f2f56b827f0f5a56ad9e3341d30
PiterZanin <piterzanin@gmail.com>
2021-12-03 11:01:00 +0100

    Added method getAmministratoreByID in AdminFacade

--- GestioneCorsi/src/main/java/com/gestionecorsi/businesscomponent/AdminFacade.java
+++ GestioneCorsi/src/main/java/com/gestionecorsi/businesscomponent/AdminFacade.java
@@ -4,30 +4,38 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import com.gestionecorsi.architecture.dao.DAOException;
+import com.gestionecorsi.businesscomponent.model.Amministratore;
 import com.gestionecorsi.businesscomponent.model.Docente;
 
 public class AdminFacade {
 	private static AdminFacade istanza;
-	
-	private AdminFacade() {}
-	
+
+	private AdminFacade() {
+	}
+
 	public static AdminFacade getInstance() {
-		if(istanza == null)
+		if (istanza == null)
 			istanza = new AdminFacade();
 		return istanza;
 	}
-	
-	public Docente getDocenteByID(long codDocente) 
+
+	public Docente getDocenteByID(long codDocente)
 			throws DAOException, ClassNotFoundException, FileNotFoundException, IOException {
-		
+
 		DocenteBC dBC = new DocenteBC();
 		return dBC.getByID(codDocente);
 	}
-	
-	public Docente getDocentePiuCorsi() 
+
+	public Docente getDocentePiuCorsi()
 			throws DAOException, ClassNotFoundException, FileNotFoundException, IOException {
-		
+
 		DocenteBC dBC = new DocenteBC();
 		return dBC.getDocentePiuCorsi();
+	}
+
+	public Amministratore getAmministratoreByID(long codAdmin)
+			throws ClassNotFoundException, DAOException, FileNotFoundException, IOException {
+		AmministratoreBC aBC = new AmministratoreBC();
+		return aBC.getById(codAdmin);
 	}
 }
